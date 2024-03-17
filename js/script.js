83bfd23c89dbe897f50160dba12f14768eb57854
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-17 16:53:06 +0100

    feat: construction of the grid

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emett
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emette
 // un messaggio in console con il numero della cella cliccata.
+
+
+// l'utente clicca sul btn play
+    // prendere il btn play
+    // si genera la griglia 10x10
+    
+// se l'utente clicca su una cella, essa si colora di azzurro
+// far uscire un messaggio in console col numero della cella cliccata
