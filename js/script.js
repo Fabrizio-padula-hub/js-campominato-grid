99bcc131f84739b5cf2ab235b01cd9ee448f5819
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-17 17:34:15 +0100

    feat: listening to the btn play and creating the grid

--- js/script.js
+++ js/script.js
@@ -4,10 +4,26 @@
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emette
 // un messaggio in console con il numero della cella cliccata.
 
+// prendere il btn play
+const playButton = document.getElementById('play-btn');
 
 // l'utente clicca sul btn play
-    // prendere il btn play
+playButton.addEventListener('click', function(){
+
     // si genera la griglia 10x10
+    const myGrid = document.querySelector('#grid');
+    for(let i = 1; i <= 100; i++){
+        const numberSquare = document.createElement('div');
+        numberSquare.classList.add('square');
+        numberSquare.innerHTML = `<span>${i}</span>`;
+        myGrid.append(numberSquare);
     
-// se l'utente clicca su una cella, essa si colora di azzurro
-// far uscire un messaggio in console col numero della cella cliccata
+    };
+
+    // mettere la cella in ascolto
+    // se l'utente clicca su una cella, leggere il numero e si colora di azzurro
+    // far uscire un messaggio in console col numero della cella cliccata
+
+});
+
+
